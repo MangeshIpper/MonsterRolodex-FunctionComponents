80b6ba54ff89546455aca1825a2cc5b276663da3
MangeshIpperNCI <mangesh.ipper@gmail.com>
2022-04-17 23:20:00 +0100

    Added Initial commit with changing class components to functional components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 function App() {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonster = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonster);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (e) => {
+    const searchFieldStr = e.target.value.toLowerCase();
+    setSearchField(searchFieldStr);
+  };
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="Search Monsters"
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./card.style.css";
+
+function Card({ monster }) {
+  const { id, name, email } = monster;
+
+  return (
+    <div className="card-container" key={id}>
+      <img
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        alt={`monset ${name}`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+}
+
+export default Card;
